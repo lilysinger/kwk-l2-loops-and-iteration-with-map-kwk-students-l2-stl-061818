52b5d2ce1790ba8a543679a33a7d3abf6de992d5
lilysinger <lilymsinger@gmail.com>
2018-06-21 18:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Code your solution in this file.
-const studentNames = ['Adele' , 'Beyonce' , 'Lady' , 'Madonna' , 'Rihanna' , 'Taylor']
+//const studentNames = ['Adele' , 'Beyonce' , 'Lady' , 'Madonna' , 'Rihanna' , 'Taylor']
 
+function hi(student) {
+  return student.toLowerCase();
+}
 function lowerCaseStudentNames(list){
-  studentNames
+ return list.map(hi);
 }
