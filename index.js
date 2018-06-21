257cf232aa4b540bb7c8890bccc8901384009705
lilysinger <lilymsinger@gmail.com>
2018-06-21 18:14:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file.
-let studentNames = ['Adele' , 'Beyonce' , 'Lady' , 'Madonna' , 'Rihanna' , 'Taylor']
+const studentNames = ['Adele' , 'Beyonce' , 'Lady' , 'Madonna' , 'Rihanna' , 'Taylor']
 
 function lowerCaseStudentNames(){
   studentNames
